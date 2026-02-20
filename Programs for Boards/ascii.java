5a8e70e99212e45b128aa4bc7fe92c2be356818e
sparshpanjwani-dev <sparshhpanjwani2@gmail.com>
2026-02-20 17:51:48 +0530

    Delete Programs for Boards directory

--- Programs for Boards/ascii.java
+++
@@ -1,8 +0,0 @@
-class ascii
-{
-public static void main(String ar[])throws Exception
-{
-for(int i=0;i<1000;i++)
-System.out.println(i+"\t\t\t"+(char)(i));
-}
-}
--- Programs for Boards/test.java
+++
@@ -1,8 +0,0 @@
-class test
-{
-public static void main(String ar[])
-{
-int n=016;
-System.out.print(n);
-}
-}
